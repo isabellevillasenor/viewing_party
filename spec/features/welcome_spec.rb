cf7c3ce3772ba634a746126dc290ae033544d40e
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-01 19:36:25 -0700

    added functionality to visit welcome page
    Co-authored-by: Isabelle Villasenor <45639675+isabellevillasenor@users.noreply.github.com>
    Co-authored-by: Saundra Catalina <68261312+saundracatalina@users.noreply.github.com>

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'Welcome' do
   end
 
   it 'displays a description of the application' do
-    expect(page).to have_content('an application to explore movies and create a viewing party event for you and your friends to watch a movie together')
+    expect(page).to have_content('An application to explore movies and create a viewing party event for you and your friends to watch a movie together')
   end
 
   it 'has a button to log in' do

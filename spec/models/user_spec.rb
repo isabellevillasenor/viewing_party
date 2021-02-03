2249f9319f23716dded0d6d48262ab061f68d6e2
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-02 21:13:48 -0700

    replace name validation with option to use email in place of name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,5 @@ describe User do
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
     it { should validate_presence_of :password }
-    it { should validate_presence_of :name }
   end
 end

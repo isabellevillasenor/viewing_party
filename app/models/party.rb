0ce64bfd3dd1d0bacd8f668ab43be8b52321803d
Isabelle Villasenor <villasenori@outlook.com>
2021-02-02 17:28:43 -0700

    Add All Models with Validations and Relationships
    Co-authored-by: Saundra Catalina <68261312+saundracatalina@users.noreply.github.com>
    
    Co-authored-by: Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>

---
+++ app/models/party.rb
@@ -0,0 +1,9 @@
+class Party < ApplicationRecord
+  validates :party_time, presence: true
+  validates :party_duration, numericality: { greater_than: 0 }
+
+  belongs_to :host, class_name: 'User'
+  belongs_to :movie
+  has_many :invitations, dependent: :destroy
+  has_many :party_people, through: :invitations
+end

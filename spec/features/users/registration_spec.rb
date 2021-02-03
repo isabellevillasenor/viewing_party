7c289a0c59e6353dcf89afecfe235858e57d21f9
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-02 21:12:54 -0700

    clean up registration feature spec

--- spec/features/users/registration_spec.rb
+++ spec/features/users/registration_spec.rb
@@ -1,13 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'user registration' do
+  before(:each) { visit registration_path }
+  let(:email) {Faker::Internet.email}
+  let(:password) {Faker::Internet.password}
+  let(:name) {Faker::Name.first_name}
+
+  it 'has a welcome message' do
+    expect(page).to have_content("Welcome to Viewing Party")
+    expect(page).to have_content("Please Register for an Account")
+  end
+
+  it 'has a link to the login page' do
+    expect(page).to have_link('Already Registered? Log in Here', href: login_path)
+  end
+
   it 'creates a new user' do
-    visit registration_path
-
-    email = Faker::Internet.email
-    password = Faker::Internet.password
-    name = Faker::Name.first_name
-
     fill_in('user[email]', with: email)
     fill_in('user[password]', with: password)
     fill_in('user[password_confirmation]', with: password)
@@ -20,8 +28,6 @@ RSpec.describe 'user registration' do
 
   describe 'sad paths' do
     it 'requires an email address and password' do
-      visit registration_path
-
       click_button('Register')
 
       expect(page).to have_content("Email can't be blank")
@@ -29,26 +35,16 @@ RSpec.describe 'user registration' do
     end
 
     it 'validates that the password entries match' do
-      visit registration_path
-
-      email = Faker::Internet.email
-      password = Faker::Internet.password
-
       fill_in('user[email]', with: email)
       fill_in('user[password]', with: password)
       fill_in('user[password_confirmation]', with: password.chop)
       click_button('Register')
 
-      expect(page).to have_content("Passwords do not match")
+      expect(page).to have_content("Password confirmation doesn't match")
     end
   end
 
   it 'can use an email address as a name if none is provided' do
-    visit registration_path
-
-    email = Faker::Internet.email
-    password = Faker::Internet.password
-
     fill_in('user[email]', with: email)
     fill_in('user[password]', with: password)
     fill_in('user[password_confirmation]', with: password)

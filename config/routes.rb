3e78e812cfd3707cd3e86abe9c7a0e8021965063
Isabelle Villasenor <villasenori@outlook.com>
2021-02-03 14:13:14 -0700

    Add Dashboard Index Route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
+  #make this a show so an id can be passed via params
+  resources :dashboard, only: [:index]
+
 end

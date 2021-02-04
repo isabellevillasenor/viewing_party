e09e5e93522eabcabfbf7af78f855e3f29a093ba
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-03 21:00:47 -0700

    add friend factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,5 +3,13 @@ FactoryBot.define do
     name { Faker::Name.first_name }
     email { Faker::Internet.email }
     password { Faker::Internet.password }
+
+    factory :friend do
+      transient { user { create(:user) } }
+
+      after(:create) do |friend, transient|
+        transient.user.friends << friend
+      end
+    end
   end
 end

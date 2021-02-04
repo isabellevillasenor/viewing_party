695ba8d0ac1f39950032fe5d60943e0e1d01085c
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-04 00:13:22 -0700

    add scopes for friendship statuses

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,4 +2,7 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
   enum status: { pending: 0, approved: 1 }
+
+  scope :pending, -> { where(status: 0) }
+  scope :approved, -> { where(status: 1) }
 end

1772cca0895d39cc2bb0559370b1fb8492a03845
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-04 00:33:42 -0700

    add functionality to cancel or deny friend requests

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,4 +3,9 @@ class FriendshipsController < ApplicationController
     Friendship.find(params[:id]).update(status: 1)
     redirect_to dashboard_path
   end
+
+  def destroy
+    Friendship.find(params[:id]).destroy
+    redirect_to dashboard_path
+  end
 end

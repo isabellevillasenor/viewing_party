a9c54f4d2e0c6d35b7eaab800141a4ab5d0018ad
Saundra <saundracatalina@gmail.com>
2021-02-04 19:11:14 -0700

    Delete redundant lines in test
    Co-authored-by: Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>

--- spec/features/movies/discover_spec.rb
+++ spec/features/movies/discover_spec.rb
@@ -4,8 +4,6 @@ describe 'movies discover page' do
   it 'has a Find Top Rated Movies button that redirects to the movies index page' do
     visit discover_path
 
-    expect(page).to have_button('Find Top Rated Movies')
-
     click_button 'Find Top Rated Movies'
     expect(current_path).to eq(movies_path)
   end
@@ -14,7 +12,6 @@ describe 'movies discover page' do
     visit discover_path
 
     expect(page).to have_field(:title, placeholder: 'Search by movie title')
-    expect(page).to have_button('Find Movies')
 
     fill_in :title, with: 'Phoenix'
     click_button 'Find Movies'

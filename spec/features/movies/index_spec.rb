2e7aa5fd53c718931673b733c36a455d5d7e6d22
Saundra <saundracatalina@gmail.com>
2021-02-04 19:12:51 -0700

    Add tests for button paths and page expectations
    Co-authored-by: Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -1,3 +1,35 @@
 require 'rails_helper'
 
-#test buttons hehe
+describe 'movies index page' do
+  it 'has a Find Top Rated Movies button that redirects to the movies index page' do
+    visit movies_path
+
+    click_button 'Find Top Rated Movies'
+    expect(current_path).to eq(movies_path)
+  end
+
+  it 'returns the top 40 movies' do
+    visit movies_path
+
+    click_button 'Find Top Rated Movies'
+    expect(page).to have_css('li', count: 40)
+  end
+
+  it 'has a search by movie title field with a button to Find Movies that redirects to movies index page' do
+    visit movies_path
+
+    expect(page).to have_field(:title, placeholder: 'Search by movie title')
+
+    fill_in :title, with: 'Phoenix'
+    click_button 'Find Movies'
+    expect(current_path).to eq(movies_path)
+  end
+
+  it 'has search results from entered criteria' do
+    visit movies_path
+
+    fill_in :title, with: 'Phoenix'
+    click_button 'Find Movies'
+    expect(page).to have_content('Phoenix').at_least(1)
+  end
+end

1a79482f6b256e9f57b3daab47b3df2b8a5f2fb3
Isabelle Villasenor <villasenori@outlook.com>
2021-02-05 19:38:41 -0700

    Add API Method to Movie Controller
    Co-authored-by: Saundra Catalina <68261312+saundracatalina@users.noreply.github.com>
    Co-authored-by: Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,26 @@
 class MoviesController < ApplicationController
+  def index
+    @movies = get_movies
+    #
+  end
+
   def discover
+  end
+
+
+  private
+  def get_movies
+    conn = Faraday.new(url: "https://api.themoviedb.org")
+    response = conn.get("/3/movie/top_rated?api_key=#{ENV['TMDB_API_KEY']}")
+    response2 = conn.get("/3/movie/top_rated?api_key=#{ENV['TMDB_API_KEY']}&page=2")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    json2 = JSON.parse(response2.body, symbolize_names: true)
+
+    combined_results = json[:results] + json2[:results]
+
+    combined_results.map do |result|
+      MovieProxy.new(result)
+    end
   end
 end

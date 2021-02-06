dc9f247500e115c710f183b69a82213d465ae2b1
Isabelle Villasenor <villasenori@outlook.com>
2021-02-06 12:07:06 -0700

    Add VCR to Movies Specs
    Co-authored-by: Saundra Catalina <68261312+saundracatalina@users.noreply.github.com>
    Co-authored-by: Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>

--- spec/features/movies/discover_spec.rb
+++ spec/features/movies/discover_spec.rb
@@ -1,39 +1,26 @@
 require 'rails_helper'
 
 describe 'movies discover page' do
-  before :each do
-    json_response = File.read('spec/fixtures/top_movies.json')
-    stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['TMDB_API_KEY']}").
-    to_return(status: 200, body: json_response)
-
-    json_response = File.read('spec/fixtures/top_movies.json')
-    stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['TMDB_API_KEY']}&page=2").
-    to_return(status: 200, body: json_response)
-
-    @query = 'phoenix'
-    json_response = File.read('spec/fixtures/movie_search.json')
-    stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['TMDB_API_KEY']}&query=#{@query}").
-    to_return(status: 200, body: json_response)
-
-    json_response = File.read('spec/fixtures/movie_search.json')
-    stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['TMDB_API_KEY']}&query=#{@query}&page=2").
-    to_return(status: 200, body: json_response)
-  end
-
   it 'has a Find Top Rated Movies button that redirects to the movies index page' do
-    visit discover_path
+    VCR.use_cassette('top_rated_movies') do
+      visit discover_path
 
-    click_button 'Find Top Rated Movies'
-    expect(current_path).to eq(movies_path)
+      click_button 'Find Top Rated Movies'
+      expect(current_path).to eq(movies_path)
+    end
   end
 
   it 'has a search by movie title field with a button to Find Movies that redirects to movies index page' do
-    visit discover_path
+    VCR.use_cassette('search_movies') do
+      visit discover_path
 
-    expect(page).to have_field(:title, placeholder: 'Search by movie title')
+      query = 'phoenix'
 
-    fill_in :title, with: @query
-    click_button 'Find Movies'
-    expect(current_path).to eq(movies_path)
+      expect(page).to have_field(:title, placeholder: 'Search by movie title')
+
+      fill_in :title, with: query
+      click_button 'Find Movies'
+      expect(current_path).to eq(movies_path)
+    end
   end
 end
--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -2,39 +2,47 @@ require 'rails_helper'
 
 describe 'movies index page' do
   it 'has a Find Top Rated Movies button that redirects to the movies index page' do
-    visit movies_path
+    VCR.use_cassette('top_rated_movies') do
+      visit movies_path
 
-    click_button 'Find Top Rated Movies'
-    expect(current_path).to eq(movies_path)
+      click_button 'Find Top Rated Movies'
+      expect(current_path).to eq(movies_path)
+    end
   end
 
   it 'returns the top 40 movies and their vote average' do
-    visit movies_path
-    
-    click_button 'Find Top Rated Movies'
+    VCR.use_cassette('top_rated_movies') do
+      visit movies_path
+      
+      click_button 'Find Top Rated Movies'
 
-    movie = page.all('li').first
+      movie = page.all('li').first
 
-    expect(page).to have_css('li', count: 40)
-    expect(movie.has_link?).to be true
-    expect(movie).to have_content('Vote Average:')
+      expect(page).to have_css('li', count: 40)
+      expect(movie.has_link?).to be true
+      expect(movie).to have_content('Vote Average:')
+    end
   end
 
   it 'has a search by movie title field with a button to Find Movies that redirects to movies index page' do
-    visit movies_path
+    VCR.use_cassette('search_movies') do
+      visit movies_path
 
-    expect(page).to have_field(:title, placeholder: 'Search by movie title')
+      expect(page).to have_field(:title, placeholder: 'Search by movie title')
 
-    fill_in :title, with: 'Phoenix'
-    click_button 'Find Movies'
-    expect(current_path).to eq(movies_path)
+      fill_in :title, with: 'Phoenix'
+      click_button 'Find Movies'
+      expect(current_path).to eq(movies_path)
+    end
   end
 
   it 'has search results from entered criteria' do
-    visit movies_path
+    VCR.use_cassette('search_movies') do
+      visit movies_path
 
-    fill_in :title, with: 'Phoenix'
-    click_button 'Find Movies'
-    expect(page).to have_content('Phoenix').at_least(1)
+      fill_in :title, with: 'Phoenix'
+      click_button 'Find Movies'
+      expect(page).to have_content('Phoenix').at_least(1)
+    end
   end
 end

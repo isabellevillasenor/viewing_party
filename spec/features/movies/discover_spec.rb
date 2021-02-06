398c759db6e12e04d1e1a146942d0b7d308f2a94
Isabelle Villasenor <villasenori@outlook.com>
2021-02-06 11:05:38 -0700

    Add Fixture Files and Stubs to Movies Discover Spec
    Co-authored-by: Saundra Catalina <68261312+saundracatalina@users.noreply.github.com>
    Co-authored-by: Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>

--- spec/features/movies/discover_spec.rb
+++ spec/features/movies/discover_spec.rb
@@ -1,6 +1,25 @@
 require 'rails_helper'
 
 describe 'movies discover page' do
+  before :each do
+    json_response = File.read('spec/fixtures/top_movies.json')
+    stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['TMDB_API_KEY']}").
+    to_return(status: 200, body: json_response)
+
+    json_response = File.read('spec/fixtures/top_movies.json')
+    stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['TMDB_API_KEY']}&page=2").
+    to_return(status: 200, body: json_response)
+
+    @query = 'phoenix'
+    json_response = File.read('spec/fixtures/movie_search.json')
+    stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['TMDB_API_KEY']}&query=#{@query}").
+    to_return(status: 200, body: json_response)
+
+    json_response = File.read('spec/fixtures/movie_search.json')
+    stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['TMDB_API_KEY']}&query=#{@query}&page=2").
+    to_return(status: 200, body: json_response)
+  end
+
   it 'has a Find Top Rated Movies button that redirects to the movies index page' do
     visit discover_path
 
@@ -13,7 +32,7 @@ describe 'movies discover page' do
 
     expect(page).to have_field(:title, placeholder: 'Search by movie title')
 
-    fill_in :title, with: 'Phoenix'
+    fill_in :title, with: @query
     click_button 'Find Movies'
     expect(current_path).to eq(movies_path)
   end

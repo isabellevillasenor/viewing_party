02088d0f19be800f97fe22ca83a5ac55e3a93e06
Saundra <saundracatalina@gmail.com>
2021-02-08 15:48:42 -0700

    Add test to display 10 cast members from movie
    Co-authored-by: Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
    Co-authored-by: Isabelle Villasenor <45639675+isabellevillasenor@users.noreply.github.com>

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -5,12 +5,17 @@ describe 'Movies Show Page' do
     data = File.read('spec/fixtures/movie_details.json')
     movie_data = JSON.parse(data, symbolize_names: true)
     @movie = MovieProxy.new(movie_data)
+    data = File.read('spec/fixtures/actors.json')
+    actors_data = JSON.parse(data, symbolize_names: true)
+    @actors = actors_data[:cast][0..10].map do |actor|
+      Actor.new(actor)
+    end
   end
-  
+
   it 'displays the movie details' do
     VCR.use_cassette('movie_details') do
       visit "/movies/#{@movie.id}"
-      
+
       expect(page).to have_content(@movie.title)
       expect(page).to have_content("Vote Average: #{@movie.vote_average}")
       expect(page).to have_content("Runtime: #{@movie.runtime}")
@@ -21,7 +26,7 @@ describe 'Movies Show Page' do
   it 'has a section with summary' do
     VCR.use_cassette('movie_details') do
       visit "/movies/#{@movie.id}"
-      
+
       within('#summary') do
         expect(page).to have_content(@movie.overview)
       end
@@ -33,9 +38,12 @@ describe 'Movies Show Page' do
       visit "/movies/#{@movie.id}"
 
       within('#cast') do
-      #make file to read
-        expect(page).to have_content(@actors.name)
-        expect(page).to have_content(@actors.character)
+        @actors[0..9].each do |actor|
+          expect(page).to have_content(actor.name)
+          expect(page).to have_content(actor.character)
+        end
+        expect(page).to_not have_content(@actors[10].name)
+        expect(page).to_not have_content(@actors[10].character)
       end
     end
   end
@@ -53,8 +61,8 @@ describe 'Movies Show Page' do
   end
 
   it 'has a button to create a viewing party' do
-    expect(page).to have_button('Create Viewing Party!') 
-    
+    expect(page).to have_button('Create Viewing Party!')
+
     click_button
     expect(current_path).to eq(new_party_path)
   end

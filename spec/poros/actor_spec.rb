403b711c8fd7024ad4f33736f15857c34e9f6aee
Saundra <saundracatalina@gmail.com>
2021-02-08 14:49:55 -0700

    Refactor test to use symbols instead of strings for keys
    Co-authored-by: Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
    Co-authored-by: Isabelle Villasenor <45639675+isabellevillasenor@users.noreply.github.com>

--- spec/poros/actor_spec.rb
+++ spec/poros/actor_spec.rb
@@ -2,24 +2,25 @@ require 'rails_helper'
 
 describe Actor do
   before :each do
-    @actor = Actor.new({
-      "adult": false,
-      "gender": 2,
-      "id": 2888,
-      "known_for_department": "Acting",
-      "name": "Will Smith",
-      "original_name": "Will Smith",
-      "popularity": 20.887,
-      "profile_path": "/eze9FO9VuryXLP0aF2cRqPCcibN.jpg",
-      "cast_id": 16,
-      "character": "Capt. Steven Hiller",
-      "credit_id": "52fe425bc3a36847f8017f8b",
-      "order": 0
-    })
+    @data = {
+      adult: false,
+      gender: 2,
+      id: 2888,
+      known_for_department: "Acting",
+      name: "Will Smith",
+      original_name: "Will Smith",
+      popularity: 20.887,
+      profile_path: "/eze9FO9VuryXLP0aF2cRqPCcibN.jpg",
+      cast_id: 16,
+      character: "Capt. Steven Hiller",
+      credit_id: "52fe425bc3a36847f8017f8b",
+      order: 0
+      }
+      @actor = Actor.new(@data)
   end
-  
+
   it 'exists and has attributes' do
-    expect(@actor.name).to eq('Will Smith')
-    expect(@actor.character).to eq('Capt. Steven Hiller')
+    expect(@actor).to be_an Actor
+    expect(@actor).to have_attributes(name: @data[:name], character: @data[:character])
   end
 end

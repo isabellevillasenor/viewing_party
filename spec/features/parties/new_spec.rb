d317632186af52f739db57d882a88df2736fe855
Isabelle Villasenor <villasenori@outlook.com>
2021-02-09 18:37:02 -0700

    Add Passing Start Date and Time of Party to New Party Spec
    Co-authored-by: Saundra Catalina <68261312+saundracatalina@users.noreply.github.com>

--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -11,10 +11,8 @@ describe 'New Viewing Party Page' do
       movie_data = JSON.parse(data, symbolize_names: true)
       @movie = MovieProxy.new(movie_data)
       visit "/movies/#{@movie.id}"
-      expect(page).to have_button('Create Viewing Party!')
 
-      click_button
-      expect(current_path).to eq(new_party_path)
+      click_button 'Create Viewing Party!'
   
       expect(find_field(:title).value).to eq(@movie.title)
       expect(find_field(:party_duration).value).to eq(@movie.runtime.to_s)
@@ -22,7 +20,24 @@ describe 'New Viewing Party Page' do
   end
 
   it 'allows users to select date and start time' do
+    VCR.use_cassette('movie_details') do
+      data = File.read('spec/fixtures/movie_details.json')
+      movie_data = JSON.parse(data, symbolize_names: true)
+      @movie = MovieProxy.new(movie_data)
+      visit "/movies/#{@movie.id}"
 
+      click_button 'Create Viewing Party!'
+
+
+      within("#party-date") do
+        expect(find_field(:party_time).value).to eq(Date.today.strftime('%Y-%m-%d'))
+      end
+
+      within("#party-time") do
+        fill_in :party_time, with: '08:00 PM'
+        expect(find_field(:party_time).value).to eq('08:00 PM')
+      end
+    end
   end
 
   it 'allows users to select a check box for each friend to add to the viewing party' do

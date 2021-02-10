9edffeb1dec214191f1f8a3467c30d334fc78ba3
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-09 17:15:47 -0700

    replace VCR cassettes with mocks & stubs in movies_facade_spec

--- spec/facades/movies_facade_spec.rb
+++ spec/facades/movies_facade_spec.rb
@@ -1,58 +1,75 @@
 require 'rails_helper'
 
 RSpec.describe MoviesFacade do
-  describe '.top_movies' do
-      it 'gets the top 40 movies' do
-        VCR.use_cassette('top_rated_movies') do
-        movies = MoviesFacade.top_movies
+  let(:movie_info) { {
+    title: "Independence Day",
+    vote_average: 6.8,
+    id: 602,
+    runtime: 145,
+    overview: "Will Smith and Jeff Goldblum save the world from aliens",
+    genres: [ {id: 28, name: "Action"},
+              {id: 12, name: "Adventure"},
+              {id: 28, name: "Science Fiction"}]
+    } }
 
-        expect(movies).to be_an(Array)
-        expect(movies[0]).to be_a(MovieProxy)
-        expect(movies.size).to eq(40)
-      end
+  describe '.top_movies' do
+    it 'gets the top 40 movies' do
+      movie_data = Array.new(40, movie_info)
+      allow(MovieDbService).to receive(:top_movies).and_return(movie_data)
+
+      top_movies = MoviesFacade.top_movies
+
+      expect(top_movies).to be_an(Array)
+      expect(top_movies[0]).to be_a(MovieProxy)
+      expect(top_movies.size).to eq(40)
     end
   end
 
   describe '.search_movies' do
     it 'gets up to 40 movies matching a search phrase' do
-      VCR.use_cassette('search_movies') do
-        movies = MoviesFacade.search_movies("Phoenix".parameterize)
+      movie_data = Array.new(40, movie_info)
+      allow(MovieDbService).to receive(:search_movies).and_return(movie_data)
 
-        expect(movies).to be_an(Array)
-        expect(movies[0]).to be_a(MovieProxy)
-      end
+      found_movies = MoviesFacade.search_movies("Shawshank")
+
+      expect(found_movies).to be_an(Array)
+      expect(found_movies[0]).to be_a(MovieProxy)
     end
   end
 
   describe '.movie_details' do
     it 'gets details for the specified movie' do
-      VCR.use_cassette('movie_details') do
-        movie = MoviesFacade.movie_details(324857)
+      movie_details = movie_info
+      allow(MovieDbService).to receive(:movie_details).and_return(movie_details)
+      movie = MoviesFacade.movie_details(324857)
 
-        expect(movie).to be_a(MovieProxy)
-      end
+      expect(movie).to be_a(MovieProxy)
     end
   end
 
   describe '.cast_details' do
     it 'gets the cast for the specified movie' do
-      VCR.use_cassette('movie_details') do
-        cast = MoviesFacade.cast_details(324857)
+      cast_info = {cast: [{name: "Will Smith", character: "Capt. Steven Hiller"},
+                          {name: "Jeff Goldblum", character: "David Levinson"}]}
+      allow(MovieDbService).to receive(:cast_details).and_return(cast_info)
 
-        expect(cast).to be_an(Array)
-        expect(cast[0]).to be_an(Actor)
-      end
+      cast = MoviesFacade.cast_details(324857)
+
+      expect(cast).to be_an(Array)
+      expect(cast[0]).to be_an(Actor)
     end
   end
 
   describe '.review_details' do
     it 'gets reviews for the specified movie' do
-      VCR.use_cassette('movie_details') do
-        reviews = MoviesFacade.review_details(324857)
+      review_info = [ {author: "GeneSiskel", content: "Amazing!"},
+                      {author: "RogerEbert", content: "Explosive!"}]
+      allow(MovieDbService).to receive(:review_details).and_return(review_info)
 
-        expect(reviews).to be_an(Array)
-        expect(reviews[0]).to be_a(Review)
-      end
+      reviews = MoviesFacade.review_details(324857)
+
+      expect(reviews).to be_an(Array)
+      expect(reviews[0]).to be_a(Review)
     end
   end
 end

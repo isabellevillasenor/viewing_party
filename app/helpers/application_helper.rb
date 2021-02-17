08eb27135d7d5e3ce5d75aa61f714efb951784a1
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-16 19:10:47 -0700

    add styling to movies show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def humanize_runtime(time)
+    hours = time / 60
+    minutes = time % 60
+    "#{hours} #{'Hour'.pluralize(hours)}, #{minutes} #{'minutes'.pluralize(minutes)}"
+  end
 end
